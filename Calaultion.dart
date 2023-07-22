388b39a41cac3f74f87e2df179923cb8cc486f5e
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2023-07-22 16:30:08 +0530

    Create Calaultion.dart

---
+++ Calaultion.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+
+void main()
+{
+  int a=20;
+  int b=30;
+  
+
+  print("addition is ${a+b}");
+  print("substraction is${a-b}");
+  print("multipaliction is${a*b}");
+  print("division is is${a/b}");
+}
