654bee77bafe06ced094d2744bc66c2bd2896be0
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2023-07-22 17:37:58 +0530

    Create Write a Dart program to find a Simple  Interest..dart

---
+++ Write a Dart program to find a Simple  Interest..dart
@@ -0,0 +1,13 @@
+
+void main()
+{
+  var principal = 10000;
+  double rate = 6.5;
+  var time = 2;
+  double interest;
+  
+  
+  interest = principal * rate * time /100;
+  
+  print("simple interest is $interest");
+}
