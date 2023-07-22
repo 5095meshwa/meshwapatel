bb7d250e0d24e1df5014fddd517581eafb9b9cf7
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2023-07-22 17:42:23 +0530

    Create Write a Dart program to find a user given number is even or odd.dart

---
+++ Write a Dart program to find a user given number is even or odd.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main() 
+{  
+     var x = 30;  
+     var y = 20;  
+     
+  
+    if(x > y)
+    {  
+         print("x is greater than y");     
+    } 
+     else  
+     {  
+         print("y is greater than x");  
+  
+    };  
+  
+}  
