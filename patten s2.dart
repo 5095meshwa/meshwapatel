e2e59e739b430255a2dffece6aa259b8f1dac403
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2023-07-29 18:13:10 +0530

    Create patten s2.dart

---
+++ patten s2.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main()
+{
+
+  
+  for (int i = 1; i <= 5; i++) 
+  {
+    for(int j = 5;j>=1;j--)
+    {
+      if(i>=j)
+            stdout.write(j);
+            else
+             stdout.write(" ");
+        }
+        print("");
+    }
+}
+  
