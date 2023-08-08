8a78bd6b4b1b6ce9a302e5fa507c5dc3dcedaffc
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2023-08-08 12:50:03 +0530

    Create  Write a Dart Program to illustrate the use of positional parameters by creating a function which returns the sum of passed parameters.dart

---
+++ Write a Dart Program to illustrate the use of positional parameters by creating a function which returns the sum of passed parameters.dart
@@ -0,0 +1,18 @@
+
+
+import 'dart:io';
+
+int sum(int a, int b)
+{
+  return a+b;
+} 
+void main()
+{
+
+
+  int a=20;
+  int b=30;
+   int result=sum(a,b);
+  
+   print("sum is of $a and $b is$result");
+}
