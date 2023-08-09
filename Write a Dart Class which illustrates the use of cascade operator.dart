496eca2869afa8a46005829a20d9e7ec540bcdc3
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2023-08-09 18:13:56 +0530

    Create Write a Dart Class which illustrates the use of cascade operator.dart

---
+++ Write a Dart Class which illustrates the use of cascade operator.dart
@@ -0,0 +1,20 @@
+
+import 'dart:io';
+class Person 
+{
+  String name;
+  int age;
+  
+  Person(this.name, this.age);
+  
+  void student() 
+  {
+    print("Name: $name, Age: $age");
+  }
+}
+void main() 
+{
+  Person person = Person("Meshva", 19)
+    ..age = 19
+    ..student();
+}
