69deab10900f11d39f3e14f5bd47b7345bcd11d6
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2024-02-14 18:17:28 +0530

    Update 2.dart

--- oop/2.dart
+++ oop/2.dart
@@ -23,45 +23,64 @@ void main() {
     if (choice == 1) {
       print("Enter customer details:");
       stdout.write("Customer ID: ");
+
       var custId = int.parse(stdin.readLineSync()!);
       stdout.write("Customer Name: ");
+
       var custName = stdin.readLineSync()!;
       stdout.write("Customer Contact: ");
+
       var custContact = stdin.readLineSync()!;
       customers.add(Customer(custId, custName, custContact));
+
       print("Customer $custName added with ID: $custId");
-    } else if (choice == 2) {
+    } 
+    
+    else if (choice == 2) {
+
       stdout.write("Enter customer ID: ");
       var custId = int.parse(stdin.readLineSync()!);
+
       if (custId <= customers.length) {
         var customer = customers[custId - 1];
         stdout.write("Enter product ID: ");
+
         var proId = stdin.readLineSync()!;
         stdout.write("Enter product name: ");
+
         var proName = stdin.readLineSync()!;
         stdout.write("Enter product quantity: ");
+
         var proQty = int.parse(stdin.readLineSync()!);
         stdout.write("Enter product price: ");
+
         var proPrice = double.parse(stdin.readLineSync()!);
-        customer.custCart.add({
+        customer.custCart.add(
+          {
           "pro_id": proId,
           "pro_name": proName,
           "pro_qty": proQty,
           "pro_price": proPrice
-        });
-      } else {
+        }
+        );
+      } 
+      
+      else 
+      {
         print("Invalid customer ID.");
       }
     } else if (choice == 3) {
       stdout.write("Enter customer ID: ");
       var custId = int.parse(stdin.readLineSync()!);
+
       if (custId <= customers.length) {
         var customer = customers[custId - 1];
         displayInvoice(customer);
       } else {
         print("Invalid customer ID.");
       }
-    } else if (choice == 4) {
+    } 
+    else if (choice == 4) {
       break;
     } else {
       print("Invalid choice. Please select a valid option.");
